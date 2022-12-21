94b9fdf9c83ff100d57ea932c14e9aef69965215
Stefano Calo <stefano.cal92@gmail.com>
2022-12-21 18:23:26 +0100

    Add renderCurrent function

--- src/features/Current/Current.js
+++ src/features/Current/Current.js
@@ -5,20 +5,40 @@ import './Current.css'
 
 export const Current = () => {
 
-    const forecast = useSelector((state) => state.forecast.forecast)
+    const forecast = useSelector((state) => state.forecast.forecast);
+    const isLoading = useSelector((state) => state.forecast.isLoading);
 
-    console.log(forecast.forecast.forecastday[0].day.maxtemp_c)
+
+    const renderCurrent = () => {
+
+        if(isLoading) {
+            return(
+                <div className="CurrentContainer">
+                    <p>Loading...</p>
+                </div>
+            )
+        }
+
+        if(forecast.location) {
+            return(
+                <div className="CurrentContainer">
+                    <div className="right">
+                        <h1>{forecast.location.name}</h1>
+                        <h3>{`${Math.floor(forecast.current.temp_c)}°`}</h3>
+                        <h3>H: {`${Math.floor(forecast.forecast.forecastday[0].day.maxtemp_c)}°`} | L: {`${Math.floor(forecast.forecast.forecastday[0].day.mintemp_c)}°`}</h3>
+                    </div>
+                    <div className="right">
+                        <h3>{forecast.current.condition.text}</h3>
+                    </div>
+                </div>
+            )
+        }
+        
+    }
 
     return(
-        <div className="CurrentContainer">
-            <div className="right">
-                <h1>{forecast.location.name}</h1>
-                <h3>{`${Math.floor(forecast.current.temp_c)}°`}</h3>
-            </div>
-            <div className="right">
-                <h3>{forecast.current.condition.text}</h3>
-                <h3>H: {`${Math.floor(forecast.forecast.forecastday[0].day.maxtemp_c)}°`} | L: {`${Math.floor(forecast.forecast.forecastday[0].day.mintemp_c)}°`}</h3>
-            </div>
+        <div className="Wrapper">
+           {renderCurrent()}
         </div>
     )
 }

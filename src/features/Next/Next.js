bf95ade264abbe952c8eb7d6e050ce72eeb533ac
Stefano Calo <stefano.cal92@gmail.com>
2022-12-23 17:31:27 +0100

    Add next days conditions

--- src/features/Next/Next.js
+++ src/features/Next/Next.js
@@ -13,7 +13,7 @@ export const Next = () => {
     let today = new Date();
     let now = today.getHours();
 
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
 
 
@@ -27,18 +27,36 @@ export const Next = () => {
             )
         }
         if(forecast.location){
+            let sunsetF = Math.floor(forecast.forecast.forecastday[0].astro.sunset[0] + forecast.forecast.forecastday[0].astro.sunset[1]);
+            let dawn = (Math.floor(forecast.forecast.forecastday[0].astro.sunrise[0] + forecast.forecast.forecastday[0].astro.sunrise[1]));
+            let sunsetHF = sunsetF + 12;
             let dayCode = today.getDay()
             return(
                 <>
                     {forecast.forecast.forecastday.map((day, index) => (
                         <div 
                         className="day"
+                        onClick={() => document.querySelector(`#day${index}`).classList.toggle('active')}
+                        id={`day${index}`}
                         key={index}>
                             <div className="top">
-                                <p>{dayCode + index <= 6 ? weekday[dayCode + index] : weekday[(dayCode + index) - 7]}</p>
+                                {dayCode + index === dayCode && <p>Today</p>}
+                                {dayCode + index <= 6 && dayCode + index !== dayCode && <p>{weekday[dayCode + index]}</p>}
+                                {dayCode + index > 6 && dayCode + index !== dayCode  && <p>{weekday[(dayCode + index) - 7]}</p>}
+                                {renderStatic(forecast.forecast.forecastday[index].day.condition.code, now, 100, 0)}
+                                <div className="highLowContainer">
+                                    <div className='maxMin'>
+                                        <p className="highLow">H:</p>
+                                        <p className="temp">{`${Math.floor(forecast.forecast.forecastday[index].day.maxtemp_c)}°`}</p>
+                                    </div>
+                                    <div className='maxMin'>
+                                        <p className="highLow">H:</p>
+                                        <p className="temp">{`${Math.floor(forecast.forecast.forecastday[index].day.mintemp_c)}°`}</p>
+                                    </div>
+                                </div>
 
-
-                            </div>
+                                </div>
+                                
                             <div className="bottom">
 
                             </div>
